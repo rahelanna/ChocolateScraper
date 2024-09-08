33a873d36c1c39e397980b2dc68177a1d0383b2d
rahelanna <temesvari.rachel@gmail.com>
2024-09-08 17:23:27 +0300

    add postgres to yml file

---
+++ chocolatescraper/chocolatescraper/pipelines.py
@@ -0,0 +1,137 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+import mysql.connector
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+
+class ChocolatescraperPipeline:
+    def process_item(self, item, spider):
+        return item
+
+
+class PriceToUSDPipeline:
+    gbpToUsdRate = 1.3
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        if adapter.get('price'):
+            float_price = float(adapter['price'])
+            adapter['price'] = float_price * self.gbpToUsdRate
+
+            return item
+        else:
+            raise DropItem(f"Missing price in {item}")
+
+
+class DublicatesPipeline:
+
+    def __init__(self):
+        self.names_seen = set()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        if adapter['name'] in self.names_seen:
+            raise DropItem(f"Duplicate item found: {item!r}")
+        else:
+            self.names_seen.add(adapter['name'])
+            return item
+
+
+class SaveToMySQLPipeline(object):
+
+    def __init__(self):
+        load_dotenv()
+        self.conn = mysql.connector.connect(
+            host=os.getenv('host'),
+            user=os.getenv('user'),
+            password=os.getenv('password'),
+            database=os.getenv('database'),
+            port=os.getenv('port')
+        )
+
+        self.curr = self.conn.cursor()
+
+        self.curr.execute("""
+        CREATE TABLE IF NOT EXISTS chocolate_products(
+            id int NOT NULL auto_increment PRIMARY KEY,
+            name text,
+            price DECIMAL,
+            url VARCHAR(255)
+        )
+        """)
+        self.conn.commit()
+
+    def process_item(self, item, spider):
+        self.store_db(item)
+        return item
+
+    def store_db(self, item):
+        self.curr.execute(""" insert into chocolate_products (
+        name,
+        price,
+        url
+        ) values(
+        %s,
+        %s,
+        %s)
+        """, (
+            item["name"],
+            item["price"],
+            item["url"]
+        ))
+        self.conn.commit()
+
+
+class SaveToPostgresPipeline(object):
+
+    def __init__(self):
+        load_dotenv()
+        self.conn = psycopg2.connect(
+            host=os.getenv('hostname'),
+            user='postgres',
+            password=os.getenv('pgpassword'),
+            database=os.getenv('databasename'),
+            port=os.getenv('pgpost')
+        )
+
+        self.curr = self.conn.cursor()
+
+        self.curr.execute("""
+        CREATE TABLE IF NOT EXISTS chocolate_products(
+            id SERIAL PRIMARY KEY,
+            name text,
+            price DECIMAL,
+            url VARCHAR(255)
+        )
+        """)
+
+        self.conn.commit()
+
+    def process_item(self, item, spider):
+        self.store_db(item)
+        return item
+
+    def store_db(self, item):
+        try:
+            self.curr.execute(""" 
+            INSERT INTO chocolate_products (name, price, url)
+            VALUES(%s, %s, %s)
+            """, (
+                item["name"],
+                item["price"],
+                item["url"]
+            ))
+        except BaseException as e:
+            print(e)
+        self.conn.commit()
---
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -0,0 +1,45 @@
+from typing import Iterable
+from urllib.parse import urlencode
+
+import scrapy
+from scrapy import Request
+
+from ..itemloaders import ChocolateProductLoader
+from ..items import ChocolatescraperItem
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = "a5d08dfe-d8f1-41ac-83e3-fa9bbfdfaef2"
+
+def get_proxy_url(url):
+    payload = {'api_key': API_KEY, 'url': url}
+    proxy_url = 'https://proxy.scrapeops.io/v1/' + urlencode(payload)
+    return proxy_url
+
+class ChocolatSpider(scrapy.Spider):
+    name = "chocolatespider"
+    # allowed_domains = ["chocolate.co.uk"]
+   #  start_urls = ["https://www.chocolate.co.uk/collections/all"]
+
+    def start_requests(self):
+        start_url = 'https://www.chocolate.co.uk/collections/all'
+        yield scrapy.Request(url=get_proxy_url(start_url), callback=self.parse)
+
+    def parse(self, response):
+
+        products = response.css('product-item')
+
+        for product in products:
+            chocolate = ChocolateProductLoader(item=ChocolatescraperItem(), selector=product)
+            chocolate.add_css('name', 'a.product-item-meta__title::text')
+            chocolate.add_css('price', 'span.price', re='<span class="price">\n'
+                                    '              <span class="visually-hidden">Sale price</span>(.*)</span>')
+            chocolate.add_css('url', 'div.product-item-meta a::attr(href)')
+
+            yield chocolate.load_item()
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+
+        if next_page is not None:
+            next_page_url = 'https://www.chocolate.co.uk' + next_page
+            yield response.follow(get_proxy_url(next_page_url), callback=self.parse)
